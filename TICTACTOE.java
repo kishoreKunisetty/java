293c6a590f9f589ef5e40036c35e900dfe7af4d6
Kishore <kishorekunisetty123@gmail.com>
2020-04-12 02:22:36 +0530

    made small changes
    added clear window for unix terminal.
    checks of anyone won for every step.

--- TICTACTOE.java
+++ TICTACTOE.java
@@ -2,7 +2,7 @@ import java.util.*;
 class tictactoe{
     public static final int X=1,O=-1;
     public static final int EMPTY=0;
-    private int board[][]=new int[3][3];
+    private static int board[][]=new int[3][3];
     private int player;
     public tictactoe(){
         clearBoard();
@@ -14,6 +14,8 @@ class tictactoe{
             }
         }
         player=X;
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
     public void putmark(int i,int j) throws IllegalArgumentException{
         if((i<0)||(i>2)||(j<0)||(j>2))
@@ -23,7 +25,7 @@ class tictactoe{
         board[i][j]=player;
         player=-player;
     }
-    public boolean iswin(int mark){
+    public static boolean iswin(int mark){
         return((board[0][0]+board[0][1]+board[0][2]==mark*3)||
                (board[1][0]+board[1][1]+board[1][2]==mark*3)||
                (board[2][0]+board[2][1]+board[2][2]==mark*3)||
@@ -56,18 +58,31 @@ class tictactoe{
     public static void main(String[] args ){
         Scanner sc=new Scanner(System.in);
         tictactoe game=new tictactoe();
+        boolean cut=false;
+        loop:
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                System.out.print("enter the value column:");
-                int n=sc.nextInt();
                 System.out.print("enter the value row:");
+                int n=sc.nextInt();
+                System.out.print("enter the value column:");
                 int c=sc.nextInt();
                 game.putmark(n,c);
                 System.out.println(game);
+                if(iswin(X)){
+                    System.out.println("X wins");
+                    cut=true;
+                    break loop;
+                }
+                else if(iswin(O)){
+                    System.out.println("O wins");
+                    cut=true;
+                    break loop;
+                }
             }
         }
-        int winning = game.winner();
+        if(!cut)
+        {int winning = game.winner();
         String[] outcome={"O wins","Tie","X wins"};
-        System.out.println(outcome[1+winning]);
+            System.out.println(outcome[1+winning]);}
     }
 }
